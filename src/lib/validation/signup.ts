89439651a0a4dae6bd8124bdb27f6671cb7a43db
wafiq <wafiqgithub775@gmail.com>
2024-03-07 02:42:17 +0600

    add signup page

---
+++ src/lib/validation/signup.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const signUpFormValidator = z
+  .object({
+    email: z.string().describe("Email").email({ message: "Invalid Email" }),
+    password: z
+      .string()
+      .describe("Password")
+      .min(8, { message: "Password is too short" })
+      .max(20, { message: "Password is too long" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords did not match",
+    path: ["confirmPassword"],
+  });
+
+export type signFormPayload = z.infer<typeof signUpFormValidator>;
