2a0e9cf440ab58087ceef2e63bafe57aa8c0adfd
wafiq <wafiqgithub775@gmail.com>
2024-04-10 23:30:36 +0600

    add SubscriptionModalProvider in DashboardLayout

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,9 +1,26 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react';
+import { SubscriptionModalProvider } from '@/lib/providers/subscription-modal-provider';
+import { getActiveProductsWithPrice } from '@/lib/supabase/queries';
+import { ProductWithPrice } from "@/lib/supabase/schema-type";
 
-const DashboardLayout = ({ children }: { children: ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: ReactNode }) => {
+    let productError: any;
+    let activeProductsWithPrice: ProductWithPrice[] | [] = [];
+    try {
+        const { data, error } = await getActiveProductsWithPrice();
+        activeProductsWithPrice = data;
+        if (!error) productError = error;
+    } catch (error) {
+        productError = error
+    }
+
     return (
-        <div>{children}</div>
+        <main className='flex overflow-hidden h-screen'>
+            <SubscriptionModalProvider activeProducts={activeProductsWithPrice} productError={productError}>
+                {children}
+            </SubscriptionModalProvider>
+        </main>
     )
 }
 
-export default DashboardLayout
+export default DashboardLayout;
