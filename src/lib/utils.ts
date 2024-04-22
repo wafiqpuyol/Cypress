45ef01707f4ff1ea09907de4097899de4e7b131a
wafiq <wafiqgithub775@gmail.com>
2024-04-22 23:38:07 +0600

    add getFormattedPrice utility func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Price } from "@/lib/supabase/schema-type";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getFormattedPrice = (price: Price) => {
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: price.currency || undefined,
+    minimumFractionDigits: 0,
+  }).format((price.unitAmount || 0) / 100);
+  return formattedPrice;
+};
